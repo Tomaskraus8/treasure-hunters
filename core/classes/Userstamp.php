d42ff0f5bee322564caafd66aa0f505b99ba6242
Tomáš Kraus <tomas.kraus@capsa.cz>
2025-08-29 23:24:36 +0200

    Empty project

---
+++ core/classes/Userstamp.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types = 1);
+
+	class Userstamp extends \Pho\Repository\Class\Userstamp {
+
+		public function getId(): int|string|null {
+			return null; // FIXME ActiveUser::getId()
+		}
+	}
